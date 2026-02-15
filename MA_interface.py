6e4f4634fcf0b598c303030fcf1f9264223cbfe7
emjuin <em.juin06@gmail.com>
2026-02-15 09:08:06 -0500

    Enhance hub function with planet and mass display
    Added font rendering for planet and mass values in the hub function.

--- MA_interface.py
+++ MA_interface.py
@@ -2,6 +2,8 @@ import pygame
 
 pygame.init()
 
+font = pygame.font.SysFont("Arial", 30, bold = True)
+
 screen_width = 1280
 screen_height = 720
 
@@ -9,9 +11,13 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 def hub(): #ONLY USE THIS
 
+        planet_value = "0"
+        mass_value = "10"
+
         #Load Image
-        menu_og = pygame.image.load('hub2.png').convert()
-        move_og = pygame.image.load('move2.png').convert()
+        menu_og = pygame.image.load('hub3.png').convert() #HUB
+        move_og = pygame.image.load('move2.png').convert() #MOVE
+
 
         scale = 0.5
 
@@ -43,6 +49,12 @@ def hub(): #ONLY USE THIS
 
                 screen.blit(menu, menu_rect)
                 screen.blit(move, move_rect)
+
+                planet_surf = font.render(planet_value, True, (255, 255, 255))
+                mass_surf = font.render(mass_value, True, (255, 255, 255))
+
+                screen.blit(planet_surf, (menu_rect.x + 117, menu_rect.y + 7))
+                screen.blit(mass_surf, (menu_rect.x + 100, menu_rect.y + 50))
 
                 pygame.display.flip()
 
