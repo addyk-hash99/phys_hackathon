9b78f607e30bc2c5153e9fdc93e3020e5b2716d8
Norah Njonjo <norahnjonjo@Norahs-Laptop.local>
2026-02-14 19:41:34 -0500

    updates

--- gamerules.py
+++ gamerules.py
@@ -1,3 +1,5 @@
+from physics.solarsystem import SolarSystem
+
 class GameRules:
     def __init__(self, physics):
         self.physics = physics
@@ -7,10 +9,10 @@ class GameRules:
 
     # Handling collisions manually
     def check_collisions(self):
-        for i in range(len(self.physics.planets)):
-            for j in range(i + 1, len(self.physics.planets)):
-                p1 = self.physics.planets[i]
-                p2 = self.physics.planets[j]
+        for i in range(2, len(self.physics.planets)):
+                p1 = self.physics.planets[1] # player is always at index 1
+                p2 = self.physics.planets[i]
+                
                 if p1.exists and p2.exists:
                     dx = p1.rebound_particle.x - p2.rebound_particle.x
                     dy = p1.rebound_particle.y - p2.rebound_particle.y
@@ -21,18 +23,21 @@ class GameRules:
                         # Collision detected, remove the smaller planet
                         if p1.mass < p2.mass:
                             p1.exists = False
+                            
                         else:
                             p2.exists = False
 
-    def update(self):
+    def update(self, solsys):
         # checking if player still exists
         player_exists = any(p.is_player for p in self.physics.planets)
         # checking if sun still exists
         sun_exists = any(p.type == "sun" for p in self.physics.planets)
 
         if not player_exists:
+            print("Earth got eaten! You lose!")
             self.game_over = True
 
         if not sun_exists:
+            print("You win!")
             self.win = True
             self.game_over = True
